f91cf041c21f52ea21aa68b43dcd986e4d5929ab
Kevin Lew <klew91@gmail.com>
2018-04-12 15:04:40 -0600

    fixed bug with incorrect i indexes

--- f1_analyzer.py
+++ f1_analyzer.py
@@ -1,5 +1,6 @@
 import csv
 from f1_score import f1_score
+from orderedset import OrderedSet
 
 topicSets = []
 
@@ -21,24 +22,39 @@ humanSets = topicSets[:4]
 machineSets = topicSets[4:]
 
 thresholds = []
+pairings = OrderedSet([])
 
+root_highest_f1 = 0
+bestHumanIdx = 0
 for i in range(10):
 
 	i_sum = 0
 	for index, rootSet in enumerate(humanSets):
 
 		root_sum = 0
-		for childSet in humanSets[:index] + humanSets[(index+1):]:
+		for humanIndex, childSet in enumerate(humanSets[:index] + humanSets[(index+1):]):
 
 			f1_sum = 0
-			for topic1 in rootSet[:i]:
+			for topic1 in rootSet[:i+1]:
 				highest_f1 = 0
-				for topic2 in childSet[:i]:
+				for topic2 in childSet[:i+1]:
 
 					current_f1 = f1_score(topic1[1], topic2[1])
 
 					if current_f1 > highest_f1:
 						highest_f1 = current_f1
+
+					secondTopicIdx = humanIndex
+					if index is 0:
+						secondTopicIdx += 1
+					elif index is 1 and secondTopicIdx > 0:
+						secondTopicIdx += 1
+					elif index is 2 and secondTopicIdx is 2:
+						secondTopicIdx += 1
+
+					if current_f1 > 0.3:
+						resultStr = " ".join(["Human ", str(index+1), " topic: ", topic1[0], " paired greater than 0.3 with Human ", str(secondTopicIdx+1), " topic: ", topic2[0] ])
+						pairings.add(resultStr)
 
 				f1_sum += highest_f1
 
@@ -48,10 +64,18 @@ for i in range(10):
 		root_average = root_sum/4.00
 		i_sum += root_average
 
+		if root_average > root_highest_f1:
+			root_highest_f1 = root_average
+			bestHumanIdx = index
+
 	i_average = i_sum/4.00
 	thresholds.append(i_average)
 
+print(bestHumanIdx)
+
 results = []
+bestTopics = []
+commonhuman = dict()
 
 for i in range(10):
 
@@ -59,22 +83,46 @@ for i in range(10):
 	for index, rootSet in enumerate(machineSets):
 
 		root_sum = 0
-		for childSet in humanSets:
+		root_highest_f1 = 0
+		bestTopic = ""
+		for humanIndex, childSet in enumerate(humanSets):
 
 			f1_sum = 0
-			for topic1 in rootSet[:i]:
+			for topic1 in rootSet[:i+1]:
 				highest_f1 = thresholds[i]
-				for topic2 in childSet[:i]:
+				humantopic = ""
+				for topic2 in childSet[:i+1]:
 
 					current_f1 = f1_score(topic1[1], topic2[1])
 
 					if current_f1 > highest_f1:
 						highest_f1 = current_f1
+						humantopic = topic2[0]
 
+					if current_f1 > root_highest_f1:
+						root_highest_f1 = current_f1
+						bestTopic = topic1[0]
+
+
+					if current_f1 > 0.3:
+						resultStr = " ".join(["Algorithm ", str(index+1), " topic: ", topic1[0], " paired greater than 0.3 with Human ", str(humanIndex+1), " topic: ", topic2[0] ])
+						pairings.add(resultStr)
+
+				if humantopic != "" and humantopic in commonhuman:
+					commonhuman[humantopic] += 1
+				else:
+					commonhuman[humantopic] = 1
 				f1_sum += highest_f1
+				#resultStr = " ".join(["Algorithm ", str(index+1), " topic: ", topic1[0], " had an average f1 score of ", str(highest_f1) ])
+				#results.append(resultStr)
 
 			f1_average = f1_sum/(i+1)
 			root_sum += f1_average
+
+		if bestTopic != "":
+			resultStr = " ".join(["Best topic for Algorithm ", str(index+1), " with ", str(i+1), " topics: ", bestTopic])
+			bestTopics.append(resultStr)
+
 
 		root_average = root_sum/4.00
 		resultStr = " ".join(["Algorithm ", str(index+1), " average F1 Score for ", str(i+1), " topics: ", str(root_average)])
@@ -89,14 +137,24 @@ for i in range(10):
 		results.append(resultStr)
 		i_sum += root_average
 
-	i_average = i_sum/4.00
-	resultStr = " ".join(["Average F1 Score across all algorithms for ", str(i+1), " topics: ", str(i_average), "\n"])
-	results.append(resultStr)
-
 outfile = open('results.txt', 'w')
 
 for result in results:
   outfile.write("%s\n" % result)
+
+for pairing in pairings:
+  outfile.write("%s\n" % pairing)
+
+outfile.write("\n")
+
+for topic in bestTopics:
+  outfile.write("%s\n" % topic)
+
+outfile.write("\n")
+
+for topic in commonhuman:
+	if commonhuman[topic] > 5:
+  		outfile.write("%s\n" % " ".join([topic, " paired more than 5 times with algorithm topics"]))
 
 
 
